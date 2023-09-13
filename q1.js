d886fa0c3a1b80e378405da32526fb90cedeb7a5
uriqwer <lisoman90@gmail.com>
2023-09-13 12:34:39 +0300

    Create q1.js

---
+++ q1.js
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+			alert('322 interprise alert coming in!');
+		</script>
+	</head>
+	<body>
+		обаминация
+	</body>
+</html>
